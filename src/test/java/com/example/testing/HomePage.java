cab6856d4cb5906705df1d8848ea7b81fdbfb1c1
RonyGomesK78 <gromilton7@gmail.com>
2021-06-22 16:10:09 -0100

    rename the class

--- src/test/java/com/example/testing/HomePage.java
+++ src/test/java/com/example/testing/HomePage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Test1 {
+public class HomePage {
 
         @Test
         public void verifyTitle(){
